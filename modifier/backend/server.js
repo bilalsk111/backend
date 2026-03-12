c4cab50f3c6b6d5bfd6baf8cbcd5570ac6a435e2
Bilal Shaikh <your-email@gmail.com>
2026-03-12 11:55:22 +0530

    EmotionTune

--- modifier/backend/server.js
+++ modifier/backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const app = require('./src/app')
 const connectToDB = require('./src/config/database')
 const {connectRedis} = require('./src/config/redis')
-PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 
 connectToDB()
 connectRedis()
--- modifier/backend/src/app.js
+++ modifier/backend/src/app.js
@@ -5,27 +5,26 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors')
 
 app.use(express.json())
-app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')))
 app.use(cookieParser())
 app.use(cors({
   origin: true, 
   credentials: true, 
 }))
+app.use(express.static("./public"))
 
 const authRouter = require('./routes/auth.route')
 const songrouter = require('./routes/song.route')
 const musicRouter = require("./routes/music.route");
 
-const distPath = path.join(__dirname, "../pubilc");
 
 
 app.use('/api/auth',authRouter)
 app.use('/api/songs',songrouter)
 app.use("/api/music", musicRouter);
-app.use(express.static(distPath));
 
-app.get("/{*any}", (req, res) => {
-  res.sendFile(path.join(distPath, "index.html"));
+
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname,"..","/public/index.html"));
 });
 
 
