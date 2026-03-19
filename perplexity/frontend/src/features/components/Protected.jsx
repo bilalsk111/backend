d4ebc5c599324ee3b8632b210d03e02cada29a99
Bilal Shaikh <your-email@gmail.com>
2026-03-19 18:21:49 +0530

    working

--- perplexity/frontend/src/features/components/Protected.jsx
+++ perplexity/frontend/src/features/components/Protected.jsx
@@ -5,9 +5,7 @@ import { Navigate } from 'react-router-dom'
 const Protected = ({ children }) => {
     const user = useSelector(state => state.auth.user)
     
-    // Agar sach mein user null hai, tabhi login par bhejo
     if (!user) {
-        // console.log("Protected Route ne dhakka mara!"); // Agar aapko check karna ho
         return <Navigate to="/login" replace />
     }
 
